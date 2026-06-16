5f554b91edccda6ca11433fa03103ac0e44bc679
pedroairees <pedrocoelho2209@gmail.com>
2026-06-15 22:01:31 -0300

    feature: implementa tela de recuperacao de senha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+/*import React from 'react';
 import UserView from "./views/components/logo/UserView"
 
 function App() {
   return (
     <UserView />
+  );
+}
+
+export default App;*/
+
+import RecuperarSenha from "./pages/RecuperarSenha";
+
+function App() {
+  return (
+    <RecuperarSenha />
   );
 }
 
